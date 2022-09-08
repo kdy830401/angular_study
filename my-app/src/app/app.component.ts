52a45f30d010cd6cbb08ffe2f6b072013778faa1
Kimddong <kdy12234@gmail.com>
2022-09-08 14:09:40 +0900

    interpolation

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,10 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+        <p>name2: {{ name2}}</p>
+        <p>age: {{ age }}</p>
+        <p>admin: {{ admin }}</p>
+        <p>address: {{ address.city }} {{ address.country }}</p>
+        <p>gender: {{ gender }}</p>
+        <p>sayHi(): {{ sayHi()}}</p>
+        <p>age * 10: {{ age * 10}}</p>
+        <p>age > 10: {{ age > 10 }}</p>
+        <p>'string': {{ 'string' }}</p>
+      `
 })
 export class AppComponent {
   // title을 'Hello Angular'로 수정
   title = 'Hello Angular';
+  name2 = 'Angular';
+  age = 20;
+  admin = true;
+  address = {
+      city: 'Seoul',
+      country: 'Korea'
+  };
+  gender: any;
+
+
+  sayHi() {
+      return `Hi, my name is ${ this.name2 }.`;
+  }
 }
